0296e1fd1c42bd4c5e5ee24f8b0a52338a5acc42
Joha Kim <johakim9936@gmail.com>
2021-02-12 22:21:54 -0500

    Edit Home page styling and create backend proxy server

--- joha-kim/src/components/Home/Home.js
+++ joha-kim/src/components/Home/Home.js
@@ -3,10 +3,20 @@ import "./Home.scss";
 const Home = () => {
   return (
     <section className="home">
-      <div className="introduction">
-        <h2>i am</h2>
-        <h1 className="name">JOHA KIM</h1>
-        <h2>a web developer</h2>
+      <div className="intro">
+        <div className="intro__paragraph">
+          <h1>
+            i'm <p className="highlight highlight-1">Joha</p>,
+          </h1>
+
+          <h2>
+            a <p className="highlight highlight-2">web</p>{" "}
+            <p className="highlight highlight-3">developer</p>
+          </h2>
+        </div>
+      </div>
+      <div className="img">
+        <img src="./me.png" alt="Me" />
       </div>
     </section>
   );
