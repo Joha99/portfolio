b8717d7693e76a1e4b88ecafd68e833dde5a9542
Joha Kim <johakim9936@gmail.com>
2021-02-22 23:08:31 -0500

    fix styling in interests component

--- client/src/components/Interests/Interests.js
+++ client/src/components/Interests/Interests.js
@@ -3,57 +3,25 @@ import $ from "jquery";
 import React, { useEffect, useState } from "react";
 
 const Interests = () => {
-  useEffect(() => {
-    var type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
-      radius = "17em", //distance from center
-      start = -90, //shift start from 0
-      $elements = $("li:not(:first-child)"),
-      // $elements = $("li"),
-      numberOfElements = type === 1 ? $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
-      slice = (360 * type) / numberOfElements;
-
-    $elements.each(function (i) {
-      var $self = $(this),
-        rotate = slice * i + start,
-        rotateReverse = rotate * -1;
-
-      $self.css({
-        transform:
-          "rotate(" +
-          rotate +
-          "deg) translate(" +
-          radius +
-          ") rotate(" +
-          rotateReverse +
-          "deg)",
-      });
-    });
-  }, []);
+  useEffect(() => {}, []);
+  const interests = [
+    "Docker",
+    "React Spring",
+    "GraphQL",
+    "MySQL / PostGres",
+    "CSS Animations",
+  ];
 
   return (
     <div className="interests">
+      <div className="heading">
+        <h1>tools im learning</h1>
+      </div>
+
       <ul className="tools">
-        <li>
-          <h1>tools im learning</h1>
-        </li>
-        <li>
-          <span>Docker</span>
-        </li>
-        <li>
-          <span>React.js</span>
-        </li>
-        <li>
-          <span>Postgres</span>
-        </li>
-        <li>
-          <span>MongoDB</span>
-        </li>
-        <li>
-          <span>GraphQL</span>
-        </li>
-        <li>
-          <span>jQuery</span>
-        </li>
+        {interests.map((interest, index) => (
+          <li key={index}>{interest}</li>
+        ))}
       </ul>
     </div>
   );
